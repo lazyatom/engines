7aaf771d7da643788394c11b1e33debf1d48c17d
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2005-11-17 10:40:50 +0000

    Fixed EngineMigrator class definition
    git-svn-id: http://svn.rails-engines.org/branches/engines@101 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- lib/migration_extensions.rb
+++ lib/migration_extensions.rb
@@ -17,27 +17,29 @@ end
 
 
 require 'breakpoint'
-class ::Engines::EngineMigrator < ActiveRecord::Migrator
+module ::Engines
+  class EngineMigrator < ActiveRecord::Migrator
 
-  # We need to be able to set the 'current' engine being migrated.
-  cattr_accessor :current_engine
-  cattr_accessor :schema_info_table_name
+    # We need to be able to set the 'current' engine being migrated.
+    cattr_accessor :current_engine
+    cattr_accessor :schema_info_table_name
   
-  class << self
+    class << self
 
-    def current_version
-      result = ActiveRecord::Base.connection.select_one("SELECT version FROM engine_schema_info WHERE engine_name = '#{current_engine.name}'")
-      if result
-        result["version"].to_i
-      else
-        # set the version to 0
-        ActiveRecord::Base.connection.execute("INSERT INTO engine_schema_info (version, engine_name) VALUES (0,'#{current_engine.name}')")      
-        0
-      end
-    end    
-  end
+      def current_version
+        result = ActiveRecord::Base.connection.select_one("SELECT version FROM engine_schema_info WHERE engine_name = '#{current_engine.name}'")
+        if result
+          result["version"].to_i
+        else
+          # set the version to 0
+          ActiveRecord::Base.connection.execute("INSERT INTO engine_schema_info (version, engine_name) VALUES (0,'#{current_engine.name}')")      
+          0
+        end
+      end    
+    end
 
-  def set_schema_version(version)
-    ActiveRecord::Base.connection.update("UPDATE engine_schema_info SET version = #{down? ? version.to_i - 1 : version.to_i} WHERE engine_name = '#{self.current_engine.name}'")
+    def set_schema_version(version)
+      ActiveRecord::Base.connection.update("UPDATE engine_schema_info SET version = #{down? ? version.to_i - 1 : version.to_i} WHERE engine_name = '#{self.current_engine.name}'")
+    end
   end
 end
