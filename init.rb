b71b560ca8107a178c845d4a6228bf81181514a3
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2006-03-19 22:54:13 +0000

    Clarified MIT license (Ticket #98) Updated Rake tasks to integrate smoothly with Rails 1.1 namespaces
    git-svn-id: http://svn.rails-engines.org/engines/trunk@304 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2005 James Adam
+# Copyright (c) 2006 James Adam
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -19,6 +19,13 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+#
+# = IN OTHER WORDS:
+#
+# You are free to use this software as you please, but if it breaks you'd
+# come a'cryin...
 #++
 
 
