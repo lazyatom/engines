8337c3733ba13719d890dc1900514c8626792703
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2006-12-16 17:38:10 +0000

    Removed PluginSet - it's no longer needed
    git-svn-id: http://svn.rails-engines.org/engines/branches/rails_1.2_dev@494 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- lib/engines/plugin_set.rb
+++
@@ -1,6 +0,0 @@
-class PluginSet < Set
-  # Finds plugins with the set with the given name (accepts Strings or Symbols)
-  def [](name)
-    self.find { |plugin| plugin.name.to_s == name.to_s }
-  end
-end
