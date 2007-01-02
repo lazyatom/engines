54405c8dd3b95925f2a6f2d14ab5492219108d88
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-01-02 16:03:52 +0000

    Fixed ActionMailer plugin template extensions
    git-svn-id: http://svn.rails-engines.org/engines/branches/rails_1.2_dev@516 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- lib/engines/rails_extensions/templates.rb
+++ lib/engines/rails_extensions/templates.rb
@@ -8,7 +8,6 @@ module Engines::RailsExtensions::Templates
 
     private
       def full_template_path_with_engine_additions(template_path, extension)
-
         path_in_app_directory = full_template_path_from_application(template_path, extension)
       
         # First check for this template in the application. If it exists, the user has
@@ -33,12 +32,13 @@ module Engines::RailsExtensions::Templates
       # such as when we are testing plugins with views. In this case, return "".
       def full_template_path_from_application(template_path, extension)
         if Engines.disable_application_view_loading
-          return nil
+          nil
         else
-          return full_template_path_without_engine_additions(template_path, extension)
-        end      
+          full_template_path_without_engine_additions(template_path, extension)
+        end    
       end
   end
+
 
   # Ensure that plugins can contain layouts
   module Layout
@@ -54,7 +54,52 @@ module Engines::RailsExtensions::Templates
         layout_list_without_engine_additions + Dir["{#{plugin_layouts.join(",")}}/**/*"]
       end
   end
+  
+  
+  # The way ActionMailer is coded in terms of finding templates is very restrictive, to the point
+  # where all templates for rendering must exist under the single base path. This is difficult to
+  # work around without re-coding significant parts of the action mailer code.
+  module MailTemplates
+    def self.included(base)
+      base.class_eval do
+        alias_method_chain :template_path, :engine_additions
+        alias_method_chain :render_message, :engine_additions
+        alias_method_chain :render, :engine_additions
+      end
+    end
+
+    private    
+      # Returns all possible template paths for the current mailer
+      def template_paths
+        paths = Rails.plugins.in_precidence_order.map { |p| "#{p.root}/app/views/#{mailer_name}" }
+        paths.unshift(template_path_without_engine_additions) unless Engines.disable_application_view_loading
+        paths
+      end
+    
+      # Return something that Dir[] can glob against
+      def template_path_with_engine_additions
+        "{#{template_paths.join(",")}}"
+      end
+
+      # Set the base_path for the ActionView::Base renderer to the correct path for the
+      # given template
+      def render_message_with_engine_additions(method_name, body)
+        render_message_without_engine_additions(method_name, body)
+      end
+      
+      # We've broken this up so that we can dynamically alter the base_path that ActionView
+      # is rendering from so that templates can be located from plugins.
+      def render_with_engine_additions(opts)
+        template_path_for_method = Dir["#{template_path}/#{opts[:file]}.*"].first
+        body = opts.delete(:body)
+        i = initialize_template_class(body)
+        i.base_path = File.dirname(template_path_for_method)
+        i.render(opts)
+      end
+  end
 end
+
 
 ::ActionView::Base.send(:include, Engines::RailsExtensions::Templates::ActionView)
 ::ActionController::Layout::ClassMethods.send(:include, Engines::RailsExtensions::Templates::Layout)
+::ActionMailer::Base.send(:include, Engines::RailsExtensions::Templates::MailTemplates)
