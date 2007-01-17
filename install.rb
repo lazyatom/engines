a9bbe4dd4a25d67676bb98d91c6728d1861c7673
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-01-17 16:41:04 +0000

    Tidied up some of the upgrade information
    git-svn-id: http://svn.rails-engines.org/engines/branches/rb_1.2@545 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- install.rb
+++ install.rb
@@ -1,13 +1,16 @@
-# TODO
-#
-#  * add upgrade path from engines 1.1.x
-
-unless Rails::VERSION::MAJOR >= 1 
-  unless Rails::VERSION::MINOR >= 2
-    puts <<-end_of_warning
+if Rails::VERSION::MAJOR < 1 && Rails::VERSION::MINOR < 2
+  puts <<-end_of_warning
 !!!=== IMPORTANT NOTE ===!!!
-Support for Rails < 1.2 has been dropped; if you are using Rails =< 1.1.6, please use Engines 1.1.6, available from http://svn.rails-engines.org/engines/tags/rel_1.1.6
-For more details about changes in Engines 1.2, please see the changelog or http://www.rails-engines.org
+Support for Rails < 1.2 has been dropped; if you are using Rails =< 1.1.6, 
+please use Engines 1.1.6, available from: 
+>>  http://svn.rails-engines.org/engines/tags/rel_1.1.6
+For more details about changes in Engines 1.2, please see the changelog or: 
+>>  http://www.rails-engines.org
 end_of_warning
-  end
+else
+  puts <<-end_of_message
+Thanks for download the engines plugin. If you're upgrading to the 1.2.x 
+branch of releases of this plugin, please ensure that you read and understand
+the contents of the UPGRADING file.'
+end_of_message
 end
