9c423fc793914ae33e0d6d1e08c8176a5ca85844
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-01-19 16:20:16 +0000

    Fixed the filename of the upgrade engines migration
    git-svn-id: http://svn.rails-engines.org/engines/branches/rb_1.2@558 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- generators/upgrade_engines/upgrade_engines_generator.rb
+++ generators/upgrade_engines/upgrade_engines_generator.rb
@@ -2,7 +2,7 @@ class UpgradeEnginesGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template 'upgrade_engines_migration.rb', 'db/migrate', 
-                          :migration_file_name => "upgrade_engines_to_1_2"
+                          :migration_file_name => "upgrade_engines_plugin_to_version_1_2"
     end
   end
 end
