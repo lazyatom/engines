88a8dfcbca1e6df9c5807f7817102b00935c86de
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-01-19 14:10:15 +0000

    Will the polish never end?
    git-svn-id: http://svn.rails-engines.org/engines/branches/rb_1.2@554 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- install.rb
+++ install.rb
@@ -6,12 +6,13 @@ unless Rails::VERSION::MAJOR >= 1 && Rails::VERSION::MINOR >= 2
 
          !!!=== IMPORTANT NOTE ===!!!
 
-Support for Rails < 1.2 has been dropped; if you are using Rails =< 1.1.6, 
-please use Engines 1.1.6, available from: 
+Support for Rails < 1.2 has been dropped; if you are using 
+Rails =< 1.1.6, please use Engines 1.1.6, available from: 
 
   >>  http://svn.rails-engines.org/engines/tags/rel_1.1.6
 
-For more details about changes in Engines 1.2, please see the changelog or: 
+For more details about changes in Engines 1.2, please see 
+the changelog or the website: 
 
   >>  http://www.rails-engines.org
 
@@ -19,10 +20,12 @@ end_of_warning
 else
   puts <<-end_of_message
   
-The engines plugin is now installed. Feels good, right? Yeah. You knew it would.
+The engines plugin is now installed. Feels good, right? Yeah. 
+You knew it would.
 
-Once the warm, fuzzy glow has subsided, be sure to read the contents of the README
-and UPGRADING files if you're migrating this application from Rails 1.1.x to 1.2.x.
+Once the warm, fuzzy glow has subsided, be sure to read the contents 
+of the README and UPGRADING files if you're migrating this application 
+from Rails 1.1.x to 1.2.x.
 
 Have a great day!
 end_of_message
