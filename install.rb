e719cfec27f8147f969d7ef9ef941056d10ebd61
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-01-19 14:05:44 +0000

    Tidied up the pre-Rails-1.2 warning
    git-svn-id: http://svn.rails-engines.org/engines/branches/rb_1.2@551 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- install.rb
+++ install.rb
@@ -3,12 +3,18 @@ silence_warnings { require 'rails/version' } # it may already be loaded.
 
 unless Rails::VERSION::MAJOR >= 1 && Rails::VERSION::MINOR >= 2
   puts <<-end_of_warning
-!!!=== IMPORTANT NOTE ===!!!
+
+         !!!=== IMPORTANT NOTE ===!!!
+
 Support for Rails < 1.2 has been dropped; if you are using Rails =< 1.1.6, 
 please use Engines 1.1.6, available from: 
+
   >>  http://svn.rails-engines.org/engines/tags/rel_1.1.6
+
 For more details about changes in Engines 1.2, please see the changelog or: 
+
   >>  http://www.rails-engines.org
+
 end_of_warning
 else
   puts <<-end_of_message
