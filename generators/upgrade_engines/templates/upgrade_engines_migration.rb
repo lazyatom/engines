4c9f21703013970dc5b5fdddc9f9faa1a2497eae
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-01-19 16:24:06 +0000

    Removing the upgrade_engines generator - it's more complicated to implement than to get users to just change their code.
    git-svn-id: http://svn.rails-engines.org/engines/branches/rb_1.2@559 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- generators/upgrade_engines/templates/upgrade_engines_migration.rb
+++
@@ -1,9 +0,0 @@
-class UpgradeEnginesPluginToVersion12 < ActiveRecord::Migration
-  def self.up
-    rename_table(:engine_schema_info, :plugin_schema_info) rescue nil
-  end
-
-  def self.down
-    rename_table(:plugin_schema_info, :engine_schema_info) rescue nil
-  end
-end
