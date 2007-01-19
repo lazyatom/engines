90bd788f6dbc41141eca73cd891f89a244826445
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-01-19 14:04:26 +0000

    git-svn-id: http://svn.rails-engines.org/engines/branches/rb_1.2@550 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- install.rb
+++ install.rb
@@ -1,24 +1,19 @@
-begin
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "..", "rails", "railties", "lib")
+silence_warnings { require 'rails/version' } # it may already be loaded.
 
-  $LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "..", "rails", "railties", "lib")
-  silence_warnings { require 'rails/version' }
-
-  unless Rails::VERSION::MAJOR >= 1 && Rails::VERSION::MINOR >= 2
-    puts <<-end_of_warning
+unless Rails::VERSION::MAJOR >= 1 && Rails::VERSION::MINOR >= 2
+  puts <<-end_of_warning
 !!!=== IMPORTANT NOTE ===!!!
 Support for Rails < 1.2 has been dropped; if you are using Rails =< 1.1.6, 
 please use Engines 1.1.6, available from: 
   >>  http://svn.rails-engines.org/engines/tags/rel_1.1.6
 For more details about changes in Engines 1.2, please see the changelog or: 
   >>  http://www.rails-engines.org
-  end_of_warning
-  end
-
-rescue # ... we couldn't detect the Rails version. Oh well.
-end
-
-puts <<-end_of_message
+end_of_warning
+else
+  puts <<-end_of_message
 Thanks for download the engines plugin. If you're upgrading to the 1.2.x 
 branch of releases of this plugin, please ensure that you read and understand
 the instructions in README and UPGRADING.'
 end_of_message
+end
