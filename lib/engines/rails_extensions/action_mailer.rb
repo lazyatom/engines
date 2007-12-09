bdf10acc1fbbb4762d39d9368cae4619cb44efd5
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-12-09 22:12:55 +0000

    Fixed ActionMailer extensions to work with plugin mail templates
    git-svn-id: http://svn.rails-engines.org/test/engines/rails_2.0/vendor/plugins/engines@655 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- lib/engines/rails_extensions/action_mailer.rb
+++ lib/engines/rails_extensions/action_mailer.rb
@@ -7,22 +7,45 @@
 # The MailTemplates module overrides two (private) methods from ActionMailer to enable mail 
 # templates within plugins:
 #
-# [+template_path+]  which now produces the contents of #template_paths
-# [+render+]         which now find the first matching template and creates an ActionVew::Base
-#                    instance with the correct @base_path for that template
-
+# [+template_path+]             which now produces the contents of #template_paths
+# [+initialize_template_class+] which now find the first matching template and creates 
+#                               an ActionVew::Base instance with the correct view_paths
+#
+# Ideally ActionMailer would use the same template-location logic as ActionView, and the same
+# view paths as ActionController::Base.view_paths, but it currently does not.
 module Engines::RailsExtensions::ActionMailer
   def self.included(base) #:nodoc:
     base.class_eval do
       # TODO commented this out because it seems to break ActionMailer
       # how can this be fixed?
       
-      #alias_method_chain :template_path, :engine_additions
-      alias_method_chain :render, :engine_additions
+      alias_method_chain :template_path, :engine_additions
+      alias_method_chain :initialize_template_class, :engine_additions
     end
   end
 
-  private    
+  private
+  
+    #--
+    # ActionMailer::Base#create uses two mechanisms to determine the proper template file(s)
+    # to load. Firstly, it searches within the template_root for files that much the explicit
+    # (or implicit) part encodings (like signup.text.plain.erb for the signup action). 
+    # This is how implicit multipart emails are built, by the way.
+    #
+    # Secondly, it then creates an ActionMailer::Base instance with it's view_paths parameter
+    # set to the template_root, so that ActionMailer will then take over rendering the
+    # templates.
+    #
+    # Ideally, ActionMailer would pass the same set of view paths as it gets in a normal
+    # request (i.e. ActionController::Base.view_paths), so that all possible view paths
+    # were searched. However, this seems to introduce some problems with helper modules.
+    #
+    # So instead, and because we have to fool these two independent parts of ActionMailer,
+    # we fudge with the mechanisms it uses to find the templates (via template_paths, and
+    # template_path_with_engine_additions), and then intercept the creation of the ActionView
+    # instance so we can set the view_paths (in initialize_template_class_with_engine_additions).
+    #++
+  
     # Returns all possible template paths for the current mailer, including those
     # within the loaded plugins.
     def template_paths
@@ -39,17 +62,16 @@ module Engines::RailsExtensions::ActionMailer
       "{#{template_paths.join(",")}}"
     end
 
-    # We've broken this up so that we can dynamically alter the base_path that ActionView
-    # is rendering from so that templates can be located from plugins.
-    def render_with_engine_additions(opts)
-      template_path_for_method = Dir["#{template_path}/#{opts[:file]}*"].first
-      body = opts.delete(:body)
-      if opts[:file] && opts[:file] !~ /\//
-        opts[:file] = "#{mailer_name}/#{opts[:file]}"
-      end
-      action_view_renderer = initialize_template_class(body)
-      action_view_renderer.base_path = File.dirname(template_path_for_method)
-      action_view_renderer.render(opts)
+    # Return an instance of ActionView::Base with the view paths set to all paths
+    # in ActionController::Base.view_paths (i.e. including all plugin view paths)
+    def initialize_template_class_with_engine_additions(assigns)
+      # I'd like to just return this, but I get problems finding methods in helper
+      # modules if the method implemention from the regular class is not called
+      # 
+      # ActionView::Base.new(ActionController::Base.view_paths.dup, assigns, self)
+      renderer = initialize_template_class_without_engine_additions(assigns)
+      renderer.view_paths = ActionController::Base.view_paths.dup
+      renderer
     end
 end
 
