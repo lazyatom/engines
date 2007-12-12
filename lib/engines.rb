2e6095fdacec1282b001d7de0c3adfe7364a526c
svenfuchs <svenfuchs@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-12-12 16:05:58 +0000

    added test for fixture copying in Engines::Testing moved mirror_files_from(source, dest) from Engines::Assets back to Engines
    git-svn-id: http://svn.rails-engines.org/engines/trunk@662 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- lib/engines.rb
+++ lib/engines.rb
@@ -131,5 +131,39 @@ module Engines
     def mix_code_from(*types)
       self.code_mixing_file_types += types.map { |x| x.to_s.singularize }
     end
+  
+    # A general purpose method to mirror a directory (+source+) into a destination
+    # directory, including all files and subdirectories. Files will not be mirrored
+    # if they are identical already (checked via FileUtils#identical?).
+    def mirror_files_from(source, destination)
+      return unless File.directory?(source)
+  
+      # TODO: use Rake::FileList#pathmap?    
+      source_files = Dir[source + "/**/*"]
+      source_dirs = source_files.select { |d| File.directory?(d) }
+      source_files -= source_dirs  
+  
+      source_dirs.each do |dir|
+        # strip down these paths so we have simple, relative paths we can
+        # add to the destination
+        target_dir = File.join(destination, dir.gsub(source, ''))
+        begin        
+          FileUtils.mkdir_p(target_dir)
+        rescue Exception => e
+          raise "Could not create directory #{target_dir}: \n" + e
+        end
+      end
+
+      source_files.each do |file|
+        begin
+          target = File.join(destination, file.gsub(source, ''))
+          unless File.exist?(target) && FileUtils.identical?(file, target)
+            FileUtils.cp(file, target)
+          end 
+        rescue Exception => e
+          raise "Could not copy #{file} to #{target}: \n" + e 
+        end
+      end  
+    end   
   end  
 end
--- lib/engines/assets.rb
+++ lib/engines/assets.rb
@@ -27,46 +27,12 @@ should edit the files within the <plugin_name>/assets/ directory itself.}
         return if plugin.public_directory.nil?
         begin 
           Engines.logger.debug "Attempting to copy plugin assets from '#{plugin.public_directory}' to '#{Engines.public_directory}'"
-          mirror_files_from(plugin.public_directory, File.join(Engines.public_directory, plugin.name))      
+          Engines.mirror_files_from(plugin.public_directory, File.join(Engines.public_directory, plugin.name))      
         rescue Exception => e
           Engines.logger.warn "WARNING: Couldn't create the public file structure for plugin '#{plugin.name}'; Error follows:"
           Engines.logger.warn e
         end
       end
-  
-      # A general purpose method to mirror a directory (+source+) into a destination
-      # directory, including all files and subdirectories. Files will not be mirrored
-      # if they are identical already (checked via FileUtils#identical?).
-      def mirror_files_from(source, destination)
-        return unless File.directory?(source)
-    
-        # TODO: use Rake::FileList#pathmap?    
-        source_files = Dir[source + "/**/*"]
-        source_dirs = source_files.select { |d| File.directory?(d) }
-        source_files -= source_dirs  
-    
-        source_dirs.each do |dir|
-          # strip down these paths so we have simple, relative paths we can
-          # add to the destination
-          target_dir = File.join(destination, dir.gsub(source, ''))
-          begin        
-            FileUtils.mkdir_p(target_dir)
-          rescue Exception => e
-            raise "Could not create directory #{target_dir}: \n" + e
-          end
-        end
-
-        source_files.each do |file|
-          begin
-            target = File.join(destination, file.gsub(source, ''))
-            unless File.exist?(target) && FileUtils.identical?(file, target)
-              FileUtils.cp(file, target)
-            end 
-          rescue Exception => e
-            raise "Could not copy #{file} to #{target}: \n" + e 
-          end
-        end  
-      end   
     end 
   end
 end
