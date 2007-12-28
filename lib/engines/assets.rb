2d46fdfe3e29460836559fda71cb6a14d1c95c4f
lazyatom <lazyatom@61cc408d-6c04-0410-aff7-b3608aa29ce9>
2007-12-28 23:38:46 +0000

    Clarified message in generated plugin_assets/README file
    git-svn-id: http://svn.rails-engines.org/engines/trunk@664 61cc408d-6c04-0410-aff7-b3608aa29ce9

--- lib/engines/assets.rb
+++ lib/engines/assets.rb
@@ -1,9 +1,9 @@
 module Engines
   module Assets    
     class << self      
-      @@readme = %{Files in this directory are automatically generated from your Rails Engines.
-They are copied from the 'public' directories of each engine into this directory
-each time Rails starts (server, console... any time 'start_engine' is called).
+      @@readme = %{Files in this directory are automatically generated from your plugins.
+They are copied from the 'assets' directories of each plugin into this directory
+each time Rails starts (script/server, script/console... and so on).
 Any edits you make will NOT persist across the next server restart; instead you
 should edit the files within the <plugin_name>/assets/ directory itself.}     
        
