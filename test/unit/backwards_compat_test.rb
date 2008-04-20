5de251c57d14961c280d31a625cee8ce2354c4aa
James Adam <james@lazyatom.com>
2008-04-20 13:32:46 +0100

    Fixed naming in the backwards compatibility test case.

--- test/unit/backwards_compat_test.rb
+++ test/unit/backwards_compat_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class ModelAndLibTest < Test::Unit::TestCase
-  def test_rails_module_should_relay_to_engines_plugins
+class BackwardsCompatibilityTest < Test::Unit::TestCase
+  def test_rails_module_plugin_method_should_delegate_to_engines_plugins
     assert_nothing_raised { Rails.plugins }
     assert_equal Engines.plugins, Rails.plugins 
   end
